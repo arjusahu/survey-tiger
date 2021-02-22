87ab306cbd7638fd9778fa8aa85215248b072f6e
arjusahu <arjusahu12@gmail.com>
2021-02-22 18:41:31 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import {
-  Switch,
-  Route
-} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from "./Components/Navbar"
+import { Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import Result from "./Pages/Result";
 function App() {
   return (
     <div>
-     <Navbar />
-     <Switch>
-     <Route exact path="/" component={Home} />
-     <Route exact path="/result" component={Result} />
-          </Switch>
+      <Navbar />
+      <Home />
     </div>
   );
 }
